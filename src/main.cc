d74f2486db46968828b7e012db7a6574f23e065a
qiantao1001 <mrqiantao@163.com>
2019-04-11 18:14:20 +0800

    Update README

--- src/main.cc
+++ src/main.cc
@@ -11,7 +11,9 @@ constexpr int kMaxNumber = 9;
 class Number {
  public:
   explicit Number(int number) : number_(number) {}
+
   inline int Value() const { return number_; }
+
   friend std::stringstream &operator<<(std::stringstream &out,
                                        const Number &number) {
     out << number.Value();
@@ -24,14 +26,18 @@ class Number {
 
 int main(int argc, char const *argv[]) {
   ::alg::combination::ElementContainer<Number> elements;
+  // generate the test data
   for (int i = kMinNumber; i <= kMaxNumber; i++) {
     elements.emplace_back(i);
   }
+  // get all combinations
   auto &&combination_list =
       ::alg::combination::GetCombination(elements, kSelectSize);
+  // print all combinations
   for (auto &&iec : combination_list) {
     std::cout << alg::combination::ToString(iec) << std::endl;
   }
+  // print size of all combinations
   std::cout << "combination size: " << combination_list.size() << std::endl;
   return 0;
 }
